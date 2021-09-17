4583c30e315a8cdce42f49feb37dac75db47b28d
Yaozhenghang Ma <Y_Ma@localhost.localdomain>
2021-09-17 08:10:17 -0400

    remark ignored file

---
+++ MC.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Site {
+public:
+    vector<int> coordinate = {0, 0, 0};
+    vector<double> spin = {0, 0, 0};
+    float spin_scaling = 0;
+    double magnetic_factor = 0;
+};
+
+int main() {
+    return 0;
+}
