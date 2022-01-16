b6834eb631c421d32784b9237b2ba37ce42f0aaf
Yaozhenghang Ma <yaozhenghang.ma@gmail.com>
2022-01-16 15:00:22 +0800

    update

--- script/plot.py
+++ script/plot.py
@@ -1,209 +1,174 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+###################################################### Data preprocess
 path = 'output.txt'
 with open(path, encoding='utf-8') as f:
     data = np.loadtxt(path, delimiter='\t', skiprows=1)
 
+size = data.shape[1]
+if size==5:
+    projection = False
+else:
+    projection = True
 
+temperature = data[1:, 0]
+energy = data[1:, 1]
+Cv = data[1:, 2]
+moment = data[1:, 3]
+chi = data[1:, 4]
+if projection:
+    moment_projection = data[1:, 5]
+    chi_projection = data[1:, 6]
 
+temperature_start = np.min(temperature)
+temperature_end = np.max(temperature)
+energy_max = np.max(energy)
+energy_min = np.min(energy)
+Cv_max = np.max(Cv)
+moment_max = np.max(moment)
+chi_max = np.max(chi)
+if projection:
+    moment_projection_max = np.max(moment_projection)
+    chi_projection_max = np.max(chi_projection)
+
+###################################################### Font and size
 font = {'family' : 'Times New Roman',
-'weight' : 'bold',
+'weight' : 'regular',
 'size' : 23
 }
 
-################################# moment
-# More versatile wrapper
-fig, host = plt.subplots(figsize=(8,5)) # (width, height) in inches
-par1 = host.twinx()
+font_italic = {'family' : 'Times New Roman',
+'style' : "italic",
+'weight' : 'regular',
+'size' : 23
+}
 
-xmin = np.min(data[1:, 0])
-xmax = np.max(data[1:, 0])
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "serif",
+    "font.serif": ["Times"],
+})
 
-moment_max = np.max(data[1:, 3])
-Ki_max = np.max(data[1:, 4])
-    
-host.set_xlim(xmin, xmax)
-host.set_ylim(0, moment_max*1.1)
-par1.set_ylim(0, Ki_max*2.2)
-    
-host.set_xlabel(r'$T(K)$', font)
-host.set_ylabel(r'$M(\mu_B/f.u.)$', font)
+figure_size = (8,6)
+
+###################################################### Moment and magnetic susceptibility
+fig, ax = plt.subplots(figsize=figure_size) # (width, height) in inches
+par1 = ax.twinx()
+
+# tick setting
+ax.spines["left"]
+ax.tick_params(axis='both', which='major', direction='in', length=6, width=1.5, labelsize=20)
+ax.tick_params(axis='both', which='minor', direction='in', length=4, width=1.5, labelsize=20)
+par1.spines["right"]
+par1.tick_params(axis='both', which='major', direction='in', length=6, width=1.5, labelsize=20)
+par1.tick_params(axis='both', which='minor', direction='in', length=4, width=1.5, labelsize=20)
+ax.set_xlim(temperature_start, temperature_end)
+ax.set_ylim(0, moment_max)
+par1.set_ylim(0, chi_max)
+
+# label setting
+ax.set_xlabel(r'$T$ $(\mathrm{K})$', font)
+ax.set_ylabel(r'$M$ $(\mu_\mathrm{B})$', font)
 par1.set_ylabel(r'$\chi$', font)
 
-color1 = plt.cm.viridis(0)
-color2 = plt.cm.viridis(0.5)
+# frame setting
+frameSize = 1.5
+ax.spines['left'].set_linewidth(frameSize)
+ax.spines['right'].set_linewidth(frameSize)
+ax.spines['top'].set_linewidth(frameSize)
+ax.spines['bottom'].set_linewidth(frameSize)
+par1.spines['left'].set_linewidth(frameSize)
+par1.spines['right'].set_linewidth(frameSize)
+par1.spines['top'].set_linewidth(frameSize)
+par1.spines['bottom'].set_linewidth(frameSize)
 
-par1.spines['right']
-host.spines["left"]
-host.tick_params(axis='both', direction='in', labelsize=20)
-par1.tick_params(axis='both', direction='in', labelsize=20)
-
-p1, = host.plot(data[1:, 0], data[1:, 3],    color=color1, label=r'$M$')
-p2, = par1.plot(data[1:, 0], data[1:, 4],    color=color2, label=r'$\chi$')
-
-lns = [p1, p2]
-host.legend(handles=lns, loc='best', prop=font)
-
-host.yaxis.label.set_color(p1.get_color())
-par1.yaxis.label.set_color(p2.get_color())
-
-# Adjust spacings w.r.t. figsize
-fig.tight_layout()
-# Alternatively: bbox_inches='tight' within the plt.savefig function 
-#                (overwrites figsize)
-
-# Best for professional typesetting, e.g. LaTeX
-plt.savefig("moment.pdf")
-# For raster graphics use the dpi argument. E.g. '[...].png", dpi=200)'
-
-
-
-
-################################# moment x
-# More versatile wrapper
-fig, host = plt.subplots(figsize=(8,5)) # (width, height) in inches
-par1 = host.twinx()
-
-xmin = np.min(data[1:, 0])
-xmax = np.max(data[1:, 0])
-
-moment_max = np.max(data[1:, 5])
-Ki_max = np.max(data[1:, 6])
-    
-host.set_xlim(xmin, xmax)
-host.set_ylim(0, moment_max*1.1)
-par1.set_ylim(0, Ki_max*2.2)
-    
-host.set_xlabel(r'$T(K)$', font)
-host.set_ylabel(r'$M_x(\mu_B/f.u.)$', font)
-par1.set_ylabel(r'$\chi_x$', font)
-
-color1 = plt.cm.viridis(0)
-color2 = plt.cm.viridis(0.5)
-
-par1.spines['right']
-host.spines["left"]
-host.tick_params(axis='both', direction='in', labelsize=20)
-par1.tick_params(axis='both', direction='in', labelsize=20)
-
-p1, = host.plot(data[1:, 0], data[1:, 5],    color=color1, label=r'$M_x$')
-p2, = par1.plot(data[1:, 0], data[1:, 6],    color=color2, label=r'$\chi_x$')
+p1, = ax.plot(temperature, moment, color="blue", label=r'$M$')
+p2, = par1.plot(temperature, chi, color="red", label=r'$\chi$')
 
 lns = [p1, p2]
-host.legend(handles=lns, loc='best', prop=font)
+ax.legend(handles=lns, loc='best', frameon=False, handlelength=1.2, prop=font)
 
-host.yaxis.label.set_color(p1.get_color())
-par1.yaxis.label.set_color(p2.get_color())
-
-# Adjust spacings w.r.t. figsize
 fig.tight_layout()
-# Alternatively: bbox_inches='tight' within the plt.savefig function 
-#                (overwrites figsize)
+plt.savefig("moment.png", format='png', dpi=600)
 
-# Best for professional typesetting, e.g. LaTeX
-plt.savefig("moment_x.pdf")
-# For raster graphics use the dpi argument. E.g. '[...].png", dpi=200)'
+###################################################### Projected moment and magnetic susceptibility
+if projection:
+    fig, ax = plt.subplots(figsize=figure_size) # (width, height) in inches
+    par1 = ax.twinx()
 
+    # tick setting
+    ax.spines["left"]
+    ax.tick_params(axis='both', which='major', direction='in', length=6, width=1.5, labelsize=20)
+    ax.tick_params(axis='both', which='minor', direction='in', length=4, width=1.5, labelsize=20)
+    par1.spines["right"]
+    par1.tick_params(axis='both', which='major', direction='in', length=6, width=1.5, labelsize=20)
+    par1.tick_params(axis='both', which='minor', direction='in', length=4, width=1.5, labelsize=20)
+    ax.set_xlim(temperature_start, temperature_end)
+    ax.set_ylim(0, moment_projection_max)
+    par1.set_ylim(0, chi_projection_max)
 
+    # label setting
+    ax.set_xlabel(r'$T$ $(\mathrm{K})$', font)
+    ax.set_ylabel(r'$M$ $(\mu_\mathrm{B})$', font)
+    par1.set_ylabel(r'$\chi$', font)
 
-################################# moment y
-# More versatile wrapper
-fig, host = plt.subplots(figsize=(8,5)) # (width, height) in inches
-par1 = host.twinx()
+    # frame setting
+    frameSize = 1.5
+    ax.spines['left'].set_linewidth(frameSize)
+    ax.spines['right'].set_linewidth(frameSize)
+    ax.spines['top'].set_linewidth(frameSize)
+    ax.spines['bottom'].set_linewidth(frameSize)
+    par1.spines['left'].set_linewidth(frameSize)
+    par1.spines['right'].set_linewidth(frameSize)
+    par1.spines['top'].set_linewidth(frameSize)
+    par1.spines['bottom'].set_linewidth(frameSize)
 
-xmin = np.min(data[1:, 0])
-xmax = np.max(data[1:, 0])
+    p1, = ax.plot(temperature, moment_projection, color="blue", label=r'$M$')
+    p2, = par1.plot(temperature, chi_projection, color="red", label=r'$\chi$')
 
-moment_max = np.max(data[1:, 7])
-Ki_max = np.max(data[1:, 8])
-    
-host.set_xlim(xmin, xmax)
-host.set_ylim(0, moment_max*1.1)
-par1.set_ylim(0, Ki_max*2.2)
-    
-host.set_xlabel(r'$T(K)$', font)
-host.set_ylabel(r'$M_y(\mu_B/f.u.)$', font)
-par1.set_ylabel(r'$\chi_y$', font)
+    lns = [p1, p2]
+    ax.legend(handles=lns, loc='best', frameon=False, handlelength=1.2, prop=font)
 
-color1 = plt.cm.viridis(0)
-color2 = plt.cm.viridis(0.5)
+    fig.tight_layout()
+    plt.savefig("projected_moment.png", format='png', dpi=600)
 
-par1.spines['right']
-host.spines["left"]
-host.tick_params(axis='both', direction='in', labelsize=20)
-par1.tick_params(axis='both', direction='in', labelsize=20)
+###################################################### Energy and specific heat
+fig, ax = plt.subplots(figsize=figure_size) # (width, height) in inches
+par1 = ax.twinx()
 
-p1, = host.plot(data[1:, 0], data[1:, 7],    color=color1, label=r'$M_y$')
-p2, = par1.plot(data[1:, 0], data[1:, 8],    color=color2, label=r'$\chi_y$')
+# tick setting
+ax.spines["left"]
+ax.tick_params(axis='both', which='major', direction='in', length=6, width=1.5, labelsize=20)
+ax.tick_params(axis='both', which='minor', direction='in', length=4, width=1.5, labelsize=20)
+par1.spines["right"]
+par1.tick_params(axis='both', which='major', direction='in', length=6, width=1.5, labelsize=20)
+par1.tick_params(axis='both', which='minor', direction='in', length=4, width=1.5, labelsize=20)
+ax.set_xlim(temperature_start, temperature_end)
+ax.set_ylim(energy_min, energy_max)
+par1.set_ylim(0, Cv_max)
+
+# label setting
+ax.set_xlabel(r'$T$ $(\mathrm{K})$', font)
+ax.set_ylabel(r'$E$ $(\mathrm{meV})$', font)
+par1.set_ylabel(r'$C_\mathrm{V}$', font)
+
+# frame setting
+frameSize = 1.5
+ax.spines['left'].set_linewidth(frameSize)
+ax.spines['right'].set_linewidth(frameSize)
+ax.spines['top'].set_linewidth(frameSize)
+ax.spines['bottom'].set_linewidth(frameSize)
+par1.spines['left'].set_linewidth(frameSize)
+par1.spines['right'].set_linewidth(frameSize)
+par1.spines['top'].set_linewidth(frameSize)
+par1.spines['bottom'].set_linewidth(frameSize)
+
+p1, = ax.plot(temperature, energy, color="blue", label=r'$E$')
+p2, = par1.plot(temperature, Cv, color="red", label=r'$C_\mathrm{V}$')
 
 lns = [p1, p2]
-host.legend(handles=lns, loc='best', prop=font)
+ax.legend(handles=lns, loc='best', frameon=False, handlelength=1.2, prop=font)
 
-host.yaxis.label.set_color(p1.get_color())
-par1.yaxis.label.set_color(p2.get_color())
-
-# Adjust spacings w.r.t. figsize
 fig.tight_layout()
-# Alternatively: bbox_inches='tight' within the plt.savefig function 
-#                (overwrites figsize)
-
-# Best for professional typesetting, e.g. LaTeX
-plt.savefig("moment_y.pdf")
-# For raster graphics use the dpi argument. E.g. '[...].png", dpi=200)'
-
-
-
-################################# moment z
-# More versatile wrapper
-fig, host = plt.subplots(figsize=(8,5)) # (width, height) in inches
-par1 = host.twinx()
-
-xmin = np.min(data[1:, 0])
-xmax = np.max(data[1:, 0])
-
-moment_max = np.max(data[1:, 9])
-Ki_max = np.max(data[1:, 10])
-    
-host.set_xlim(xmin, xmax)
-host.set_ylim(0, moment_max*1.1)
-par1.set_ylim(0, Ki_max*2.2)
-    
-host.set_xlabel(r'$T(K)$', font)
-host.set_ylabel(r'$M_z(\mu_B/f.u.)$', font)
-par1.set_ylabel(r'$\chi_z$', font)
-
-color1 = plt.cm.viridis(0)
-color2 = plt.cm.viridis(0.5)
-
-par1.spines['right']
-host.spines["left"]
-host.tick_params(axis='both', direction='in', labelsize=20)
-par1.tick_params(axis='both', direction='in', labelsize=20)
-
-p1, = host.plot(data[1:, 0], data[1:, 9],    color=color1, label=r'$M_z$')
-p2, = par1.plot(data[1:, 0], data[1:, 10],    color=color2, label=r'$\chi_z$')
-
-lns = [p1, p2]
-host.legend(handles=lns, loc='best', prop=font)
-
-host.yaxis.label.set_color(p1.get_color())
-par1.yaxis.label.set_color(p2.get_color())
-
-# Adjust spacings w.r.t. figsize
-fig.tight_layout()
-# Alternatively: bbox_inches='tight' within the plt.savefig function 
-#                (overwrites figsize)
-
-# Best for professional typesetting, e.g. LaTeX
-plt.savefig("moment_z.pdf")
-# For raster graphics use the dpi argument. E.g. '[...].png", dpi=200)'
-
-
-########################### Cv
-fig, host = plt.subplots(figsize=(8,5))
-host.set_xlabel(r'$T(K)$', font)
-host.set_ylabel(r'$Cv(meV/K)$', font)
-host.tick_params(axis='both', direction='in', labelsize=20)
-host.plot(data[1:, 0], data[1:, 2],    color=color1, label=r'$Cv$')
-plt.savefig("Cv.pdf")
+plt.savefig("energy.png", format='png', dpi=600)
