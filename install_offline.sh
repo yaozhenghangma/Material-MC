b28995da8423374bf7e14cebd65d2e264b2c96fa
Yaozhenghang Ma <yaozhenghang.ma@gmail.com>
2022-03-23 14:44:01 +0800

    update bash script

--- install_offline.sh
+++ install_offline.sh
@@ -1,34 +1,12 @@
 #!/bin/sh
 
-# Check installation of Xmake
-if [ $(which xmake) ];
-then
-rm -rf ./xmake
-rm -rf ./build-artifacts
-rm -rf ./xmake-repo
-else
-cd ./xmake
-make build 
-./scripts/get.sh __local__ __install_only__
-source ~/.xmake/profile
-cd ..
-rm -rf ./xmake
-mv ./build-artifacts ~/.xmake/repositories/
-mv ./xmake-repo ~/.xmake/repositories/
-fi
-
 if [ $(which MMC) ];
 then 
 # Uninstall old version
-xmake uninstall --installdir=$HOME/.local/MMC
 rm $HOME/.local/bin/MMC
-else 
-# Install packages
-xmake g --network=private
-xrepo import -i ./packages cmake scnlib fmt ctre spdlog toml++
 fi
 
 # Install MMC
-xmake -y
-xmake install -o $HOME/.local/MMC
-ln -s $HOME/.local/MMC/bin/MMC $HOME/.local/bin/MMC
+cmake . -DCMAKE_CXX_COMPILER=`which g++` -DCMAKE_C_COMPILER=`which gcc`
+cmake --build .
+ln build/MMC ~/.local/bin/MMC
